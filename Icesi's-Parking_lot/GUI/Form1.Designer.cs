b30286c164fa0d66298599360b6df674d884cad1
JuanDVera <juandaveru@gmail.com>
2020-01-25 15:49:29 -0500

    Desing of the GUI done

---
+++ Icesi's-Parking_lot/GUI/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labSearch = new System.Windows.Forms.Label();
+            this.labName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.labLastName = new System.Windows.Forms.Label();
+            this.txtBrand = new System.Windows.Forms.TextBox();
+            this.labBrand = new System.Windows.Forms.Label();
+            this.txtLicense = new System.Windows.Forms.TextBox();
+            this.labLicense = new System.Windows.Forms.Label();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.labColor = new System.Windows.Forms.Label();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.labModel = new System.Windows.Forms.Label();
+            this.labCareer = new System.Windows.Forms.Label();
+            this.txtCareer = new System.Windows.Forms.TextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.butSearch = new System.Windows.Forms.Button();
+            this.butLast = new System.Windows.Forms.Button();
+            this.butNext = new System.Windows.Forms.Button();
+            this.Edit = new System.Windows.Forms.Button();
+            this.butSave = new System.Windows.Forms.Button();
+            this.butNew = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labSearch
+            // 
+            this.labSearch.AutoSize = true;
+            this.labSearch.Location = new System.Drawing.Point(150, 7);
+            this.labSearch.Name = "labSearch";
+            this.labSearch.Size = new System.Drawing.Size(53, 17);
+            this.labSearch.TabIndex = 0;
+            this.labSearch.Text = "Search";
+            // 
+            // labName
+            // 
+            this.labName.AutoSize = true;
+            this.labName.Location = new System.Drawing.Point(30, 103);
+            this.labName.Name = "labName";
+            this.labName.Size = new System.Drawing.Size(45, 17);
+            this.labName.TabIndex = 1;
+            this.labName.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(166, 103);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(189, 22);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(166, 146);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(189, 22);
+            this.txtLastName.TabIndex = 4;
+            // 
+            // labLastName
+            // 
+            this.labLastName.AutoSize = true;
+            this.labLastName.Location = new System.Drawing.Point(30, 146);
+            this.labLastName.Name = "labLastName";
+            this.labLastName.Size = new System.Drawing.Size(74, 17);
+            this.labLastName.TabIndex = 3;
+            this.labLastName.Text = "Last name";
+            // 
+            // txtBrand
+            // 
+            this.txtBrand.Location = new System.Drawing.Point(166, 238);
+            this.txtBrand.Name = "txtBrand";
+            this.txtBrand.Size = new System.Drawing.Size(189, 22);
+            this.txtBrand.TabIndex = 8;
+            // 
+            // labBrand
+            // 
+            this.labBrand.AutoSize = true;
+            this.labBrand.Location = new System.Drawing.Point(30, 238);
+            this.labBrand.Name = "labBrand";
+            this.labBrand.Size = new System.Drawing.Size(46, 17);
+            this.labBrand.TabIndex = 7;
+            this.labBrand.Text = "Brand";
+            // 
+            // txtLicense
+            // 
+            this.txtLicense.Location = new System.Drawing.Point(166, 195);
+            this.txtLicense.Name = "txtLicense";
+            this.txtLicense.Size = new System.Drawing.Size(189, 22);
+            this.txtLicense.TabIndex = 6;
+            // 
+            // labLicense
+            // 
+            this.labLicense.AutoSize = true;
+            this.labLicense.Location = new System.Drawing.Point(30, 195);
+            this.labLicense.Name = "labLicense";
+            this.labLicense.Size = new System.Drawing.Size(93, 17);
+            this.labLicense.TabIndex = 5;
+            this.labLicense.Text = "License Plate";
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(166, 324);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(189, 22);
+            this.txtColor.TabIndex = 12;
+            // 
+            // labColor
+            // 
+            this.labColor.AutoSize = true;
+            this.labColor.Location = new System.Drawing.Point(30, 324);
+            this.labColor.Name = "labColor";
+            this.labColor.Size = new System.Drawing.Size(41, 17);
+            this.labColor.TabIndex = 11;
+            this.labColor.Text = "Color";
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(166, 281);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(189, 22);
+            this.txtModel.TabIndex = 10;
+            // 
+            // labModel
+            // 
+            this.labModel.AutoSize = true;
+            this.labModel.Location = new System.Drawing.Point(30, 281);
+            this.labModel.Name = "labModel";
+            this.labModel.Size = new System.Drawing.Size(46, 17);
+            this.labModel.TabIndex = 9;
+            this.labModel.Text = "Model";
+            // 
+            // labCareer
+            // 
+            this.labCareer.AutoSize = true;
+            this.labCareer.Location = new System.Drawing.Point(30, 369);
+            this.labCareer.Name = "labCareer";
+            this.labCareer.Size = new System.Drawing.Size(51, 17);
+            this.labCareer.TabIndex = 13;
+            this.labCareer.Text = "Career";
+            // 
+            // txtCareer
+            // 
+            this.txtCareer.Location = new System.Drawing.Point(166, 369);
+            this.txtCareer.Name = "txtCareer";
+            this.txtCareer.Size = new System.Drawing.Size(189, 22);
+            this.txtCareer.TabIndex = 14;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtSearch.Location = new System.Drawing.Point(119, 35);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(106, 22);
+            this.txtSearch.TabIndex = 15;
+            this.txtSearch.Text = "License Plate";
+            this.txtSearch.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // butSearch
+            // 
+            this.butSearch.Location = new System.Drawing.Point(231, 34);
+            this.butSearch.Name = "butSearch";
+            this.butSearch.Size = new System.Drawing.Size(41, 23);
+            this.butSearch.TabIndex = 16;
+            this.butSearch.Text = "Go!";
+            this.butSearch.UseVisualStyleBackColor = true;
+            // 
+            // butLast
+            // 
+            this.butLast.Location = new System.Drawing.Point(119, 427);
+            this.butLast.Name = "butLast";
+            this.butLast.Size = new System.Drawing.Size(75, 23);
+            this.butLast.TabIndex = 17;
+            this.butLast.Text = "Last";
+            this.butLast.UseVisualStyleBackColor = true;
+            // 
+            // butNext
+            // 
+            this.butNext.Location = new System.Drawing.Point(200, 427);
+            this.butNext.Name = "butNext";
+            this.butNext.Size = new System.Drawing.Size(75, 23);
+            this.butNext.TabIndex = 18;
+            this.butNext.Text = "Next";
+            this.butNext.UseVisualStyleBackColor = true;
+            // 
+            // Edit
+            // 
+            this.Edit.Location = new System.Drawing.Point(74, 469);
+            this.Edit.Name = "Edit";
+            this.Edit.Size = new System.Drawing.Size(75, 23);
+            this.Edit.TabIndex = 19;
+            this.Edit.Text = "Edit";
+            this.Edit.UseVisualStyleBackColor = true;
+            // 
+            // butSave
+            // 
+            this.butSave.Location = new System.Drawing.Point(236, 469);
+            this.butSave.Name = "butSave";
+            this.butSave.Size = new System.Drawing.Size(75, 23);
+            this.butSave.TabIndex = 20;
+            this.butSave.Text = "Save";
+            this.butSave.UseVisualStyleBackColor = true;
+            // 
+            // butNew
+            // 
+            this.butNew.Location = new System.Drawing.Point(155, 469);
+            this.butNew.Name = "butNew";
+            this.butNew.Size = new System.Drawing.Size(75, 23);
+            this.butNew.TabIndex = 21;
+            this.butNew.Text = "New";
+            this.butNew.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 532);
+            this.Controls.Add(this.butNew);
+            this.Controls.Add(this.butSave);
+            this.Controls.Add(this.Edit);
+            this.Controls.Add(this.butNext);
+            this.Controls.Add(this.butLast);
+            this.Controls.Add(this.butSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.txtCareer);
+            this.Controls.Add(this.labCareer);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.labColor);
+            this.Controls.Add(this.txtModel);
+            this.Controls.Add(this.labModel);
+            this.Controls.Add(this.txtBrand);
+            this.Controls.Add(this.labBrand);
+            this.Controls.Add(this.txtLicense);
+            this.Controls.Add(this.labLicense);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.labLastName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.labName);
+            this.Controls.Add(this.labSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labSearch;
+        private System.Windows.Forms.Label labName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label labLastName;
+        private System.Windows.Forms.TextBox txtBrand;
+        private System.Windows.Forms.Label labBrand;
+        private System.Windows.Forms.TextBox txtLicense;
+        private System.Windows.Forms.Label labLicense;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.Label labColor;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.Label labModel;
+        private System.Windows.Forms.Label labCareer;
+        private System.Windows.Forms.TextBox txtCareer;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button butSearch;
+        private System.Windows.Forms.Button butLast;
+        private System.Windows.Forms.Button butNext;
+        private System.Windows.Forms.Button Edit;
+        private System.Windows.Forms.Button butSave;
+        private System.Windows.Forms.Button butNew;
+    }
+}
+
